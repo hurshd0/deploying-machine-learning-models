b1075bb47bea31af84ec3116320be14a0bbe40c1
Harsh Desai <hurshd0@gmail.com>
2020-07-01 02:55:44 +0000

    hwk: assignment 3 completed

--- assignments/assignment_3/config.py
+++ assignments/assignment_3/config.py
@@ -6,10 +6,10 @@ PIPELINE_NAME = 'logistic_regression.pkl'
 
 # ======= FEATURE GROUPS =============
 
-TARGET = 
+TARGET = "survived"
 
-CATEGORICAL_VARS = 
+CATEGORICAL_VARS = ['sex', 'cabin', 'embarked', 'title']
 
-NUMERICAL_VARS = 
+NUMERICAL_VARS = ['age', 'fare']
 
-CABIN = 
+CABIN = 'cabin'
--- assignments/assignment_3/pipeline.py
+++ assignments/assignment_3/pipeline.py
@@ -9,4 +9,24 @@ import config
 titanic_pipe = Pipeline(
     # complete with the list of steps from the preprocessors file
     # and the list of variables from the config
-    )
+    [
+        ('categorical_imputer',
+            pp.CategoricalImputer(variables=config.CATEGORICAL_VARS)),
+        ('missing_indicator',
+            pp.MissingIndicator(variables=config.NUMERICAL_VARS)),
+        ('numerical_imputer',
+            pp.NumericalImputer(variables=config.NUMERICAL_VARS)),
+        ('cabin_letter_exctractor',
+            pp.ExtractFirstLetter(variables=config.CABIN)),
+        ('rare_label_encoder',
+            pp.RareLabelCategoricalEncoder(
+                tol=0.05,
+                variables=config.CATEGORICAL_VARS)),
+        ('categorical_encoder',
+            pp.CategoricalEncoder(variables=config.CATEGORICAL_VARS)),
+        ('scaler',
+            StandardScaler()),
+        ('Linear_model', LogisticRegression(C=0.0005, random_state=0))
+    ]
+
+)
